9ce4217aaacf8e51a84c74e002d401565050de78
lguarda <leoman037@live.fr>
2026-01-07 00:31:19 +0100

    feat: introduce json config to be able to configure the behavior of the mod

---
+++ SmartCursor/SmartCursorConfig.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using Vintagestory.API.Common;
+
+// thanks to
+// https://github.com/Xandu93/VSMods/blob/master/mods/xinvtweaks/src/InvTweaksConfig.cs
+// for the inspiration
+
+namespace SmartCursor {
+public class SmartCursorConfig {
+    public Dictionary<string, string[]> materialTools = new Dictionary<string, string[]>();
+    public Dictionary<string, string[]> domainTools = new Dictionary<string, string[]>();
+
+    public SmartCursorConfig() {
+        domainTools = new() {
+            // We don't want Scythe for mushroom
+            ["mushroom"] = new[] { "Knife" },
+            // Actualy most of the time hammer are not in inventory so let's still
+            // pick the pickaxe first
+            // but go to hammer if there's no pickaxe
+            ["anvil"] = new[] { "Pickaxe", "Hammer" },
+        };
+        materialTools = new() {
+            ["Gravel"] = new[] { "Shovel" },
+            ["Sand"] = new[] { "Shovel" },
+            ["Snow"] = new[] { "Shovel" },
+            ["Soil"] = new[] { "Shovel" },
+
+            ["Metal"] = new[] { "Pickaxe" },
+            ["Ore"] = new[] { "Pickaxe" },
+            ["Stone"] = new[] { "Pickaxe" },
+            ["Ice"] = new[] { "Pickaxe" },
+            ["Glass"] = new[] { "Pickaxe" },
+            ["Brick"] = new[] { "Pickaxe" },
+            ["Ceramic"] = new[] { "Pickaxe" },
+
+            ["Wood"] = new[] { "Axe" },
+            ["Plant"] = new[] { "Scythe", "Knife" },
+            ["Leaves"] = new[] { "Shears", "Axe" },
+            // Liquid = 8
+            // TODO Add Bucket and bowl
+            // Air = 0
+            // Cloth = 16
+            // Fire = 19
+            // Lava = 17
+            // Mantle = 12
+            // Meta = 20
+            // Other = 21
+        };
+    }
+}
+}
--- SmartCursor/SmartCursorModSystem.cs
+++ SmartCursor/SmartCursorModSystem.cs
@@ -2,262 +2,291 @@
 using Vintagestory.API.Common;
 using Vintagestory.API.Config;
 using Vintagestory.API.Common.Entities;
+using System;
+using System.Collections.Generic;
 
 namespace SmartCursor {
 
 public class SmartCursorModSystem : ModSystem {
-  const string HOTKEY_SMARTCURSOR = "smartcursor";
-  const string HOTKEY_SMARTCURSOR_TOGGLE = "smartcursor toggle";
+    const string HOTKEY_SMARTCURSOR = "smartcursor";
+    const string HOTKEY_SMARTCURSOR_TOGGLE = "smartcursor toggle";
 
-  ICoreClientAPI _capi;
-  int _savedSlotIndex;
-  string _savedSlotInventoryName;
-  int _savedActiveSlotIndex;
-  bool _isSmartToolHeld;
-  bool _isToggleMode;
-  long _listener = -1;
+    ICoreClientAPI _capi;
+    int _savedSlotIndex;
+    string _savedSlotInventoryName;
+    int _savedActiveSlotIndex;
+    bool _isSmartToolHeld;
+    bool _isToggleMode;
+    long _listener = -1;
+    SmartCursorConfig _config;
+    Dictionary<EnumBlockMaterial, EnumTool[]> _materialTools;
+    Dictionary<string, EnumTool[]> _domainTools;
 
-  private void RegisterKey(string keyCode, GlKeys key) {
-    _capi.Input.RegisterHotKey(keyCode, $"Smart cursor key: {keyCode}", key,
-                               HotkeyType.GUIOrOtherControls);
-    _capi.Input.SetHotKeyHandler(keyCode, (_) => true);
-  }
-
-  private void HotKeyListener(string hotkeycode, KeyCombination keyComb) {
-    switch (hotkeycode) {
-    case HOTKEY_SMARTCURSOR:
-      StartSmartCursor(false);
-      break;
-    case HOTKEY_SMARTCURSOR_TOGGLE:
-      StartSmartCursor(true);
-      break;
-    }
-  }
-
-  public override void StartClientSide(ICoreClientAPI api) {
-    Mod.Logger.Notification("SmartCursor starting");
-    _isSmartToolHeld = false;
-    _capi = api;
-
-    RegisterKey(HOTKEY_SMARTCURSOR, GlKeys.R);
-    RegisterKey(HOTKEY_SMARTCURSOR_TOGGLE, GlKeys.Tilde);
-    _capi.Input.AddHotkeyListener(HotKeyListener);
-  }
-
-  private void SmartToolStopListListener(float t) {
-    // This should not happen but don't do anything if there's no current smart
-    // tool held
-    if (!_isSmartToolHeld) {
-      return;
-    }
-    int currentActiveSlotIndex =
-        _capi.World.Player.InventoryManager.ActiveHotbarSlotNumber;
-
-    // To avoid confusion when active bar change disable the smart tool
-    // or when not in toggle mode and hotkey was released
-    if (currentActiveSlotIndex != _savedActiveSlotIndex ||
-        (!_isToggleMode && !_capi.Input.IsHotKeyPressed(HOTKEY_SMARTCURSOR))) {
-      PopTool();
-    }
-  }
-
-  private EnumTool[] SmartToolSelectorEntity() {
-    EntitySelection es = _capi.World.Player.CurrentEntitySelection;
-
-    if (es != null) {
-      Entity entity = es.Entity;
-      if (!entity.Alive) {
-        return [EnumTool.Knife];
-      }
-    }
-    return [];
-  }
-
-  // This function return tool based on targeted block
-  private EnumTool[] SmartToolSelector() {
-    // TODO clean this or maybe wait for json config
-    EnumTool[] tool = SmartToolSelectorEntity();
-    if (tool.Length > 0) {
-      return tool;
-    }
-    BlockSelection bs = _capi.World.Player.CurrentBlockSelection;
-
-    if (bs == null) {
-      return [];
+    private void RegisterKey(string keyCode, GlKeys key) {
+        _capi.Input.RegisterHotKey(keyCode, $"Smart cursor key: {keyCode}", key, HotkeyType.GUIOrOtherControls);
+        _capi.Input.SetHotKeyHandler(keyCode, (_) => true);
     }
 
-    Block block = _capi.World.BlockAccessor.GetBlock(bs.Position);
-    // _capi.ShowChatMessage($"key {block.BlockMaterial}");
-    switch (block.BlockMaterial) {
-    case EnumBlockMaterial.Gravel:
-    case EnumBlockMaterial.Sand:
-    case EnumBlockMaterial.Snow:
-    case EnumBlockMaterial.Soil:
-      return [EnumTool.Shovel];
-    case EnumBlockMaterial.Metal:
-      return [EnumTool.Hammer];
-    case EnumBlockMaterial.Ore:
-    case EnumBlockMaterial.Stone:
-    case EnumBlockMaterial.Ice:
-    case EnumBlockMaterial.Glass:
-    case EnumBlockMaterial.Brick:
-    case EnumBlockMaterial.Ceramic:
-      return [EnumTool.Pickaxe];
-    case EnumBlockMaterial.Plant:
-      return [EnumTool.Scythe, EnumTool.Knife];
-    case EnumBlockMaterial.Wood:
-      return [EnumTool.Axe];
-    case EnumBlockMaterial.Leaves:
-      return [EnumTool.Shears, EnumTool.Axe];
-    // Liquid = 8
-    // TODO Add Bucket and bowl
-    //
-    // Air = 0
-    // Cloth = 16
-    // Fire = 19
-    // Lava = 17
-    // Mantle = 12
-    // Meta = 20
-    // Other = 21
-    default:
-      return [];
-    }
-  }
-
-  private bool SwapItemSlot(ItemSlot a, ItemSlot b) {
-    // Until i know how to do this properly right now i'm using the
-    // mouseItemSlot as temporary item holder during the swap
-    ItemSlot mouseItemSlot = _capi.World.Player.InventoryManager.MouseItemSlot;
-    if (!mouseItemSlot.Empty) {
-      return false;
+    private void HotKeyListener(string hotkeycode, KeyCombination keyComb) {
+        switch (hotkeycode) {
+        case HOTKEY_SMARTCURSOR:
+            StartSmartCursor(false);
+            break;
+        case HOTKEY_SMARTCURSOR_TOGGLE:
+            StartSmartCursor(true);
+            break;
+        }
     }
 
-    // I really need to find a better way to do this it's seems to work
-    // Now this two move operation are here to account for empty item slot as
-    // well as slot with multiple stack size maybe try with max stack size and
-    // call it a day?
-    ItemStackMoveOperation op_a =
-        new ItemStackMoveOperation(_capi.World, EnumMouseButton.None, 0,
-                                   EnumMergePriority.AutoMerge, a.StackSize);
-
-    ItemStackMoveOperation op_b =
-        new ItemStackMoveOperation(_capi.World, EnumMouseButton.None, 0,
-                                   EnumMergePriority.AutoMerge, b.StackSize);
-
-    object obj = _capi.World.Player.InventoryManager.TryTransferTo(
-        a, mouseItemSlot, ref op_a);
-    if (obj != null) {
-      _capi.Network.SendPacketClient(obj);
+    private void LoadConfig(string path) {
+        Mod.Logger.Notification("MODS OMG 1");
+        try {
+        Mod.Logger.Notification("MODS OMG 2");
+            _config = _capi.LoadModConfig<SmartCursorConfig>(path);
+        Mod.Logger.Notification($"MODS OMG 3{_config.materialTools["Plant"][0]}");
+        } catch (Exception) {
+            _config = null;
+        }
+        if (_config == null) {
+        Mod.Logger.Notification("MODS OMG 4");
+            _config = new SmartCursorConfig();
+        Mod.Logger.Notification("MODS OMG 5");
+        }
+        _capi.StoreModConfig(_config, path);
     }
 
-    obj = _capi.World.Player.InventoryManager.TryTransferTo(b, a, ref op_b);
-    if (obj != null) {
-      _capi.Network.SendPacketClient(obj);
+    private EnumTool[] StringsToEnumToolArray(string[] tools) {
+        EnumTool[] result = new EnumTool[tools.Length];
+
+        for (int i = 0; i < tools.Length; i++) {
+            if (!Enum.TryParse(tools[i], ignoreCase: true, out result[i])) {
+                Mod.Logger.Notification($"Invalid tool enum: {tools[i]}");
+            }
+        }
+
+        return result;
+    }
+    private void parseMaterialTools() {
+        _materialTools = new Dictionary<EnumBlockMaterial, EnumTool[]>();
+        foreach (var kv in _config.materialTools) {
+            EnumBlockMaterial material;
+            if (Enum.TryParse(kv.Key, ignoreCase: true, out material)) {
+                _materialTools[material] = StringsToEnumToolArray(kv.Value);
+            }
+        }
+        Mod.Logger.Notification($"MODS OMG 6{_materialTools[EnumBlockMaterial.Plant][0]}");
     }
 
-    obj = _capi.World.Player.InventoryManager.TryTransferTo(mouseItemSlot, b,
-                                                            ref op_a);
-    if (obj != null) {
-      _capi.Network.SendPacketClient(obj);
+    private void parseDomainTools() {
+        _domainTools = new Dictionary<string, EnumTool[]>();
+
+        foreach (var kv in _config.domainTools) {
+            _domainTools[kv.Key] = StringsToEnumToolArray(kv.Value);
+        }
     }
 
-    return true;
-  }
+    public override void StartClientSide(ICoreClientAPI api) {
+        Mod.Logger.Notification("SmartCursor starting");
+        _isSmartToolHeld = false;
+        _capi = api;
 
-  private bool IsRightTool(ItemSlot slot, EnumTool toolType) {
-    EnumTool? currentTool = slot?.Itemstack?.Collectible?.Tool;
-    return currentTool == toolType;
-  }
+        LoadConfig("smartcursor.json");
+        parseDomainTools();
+        parseMaterialTools();
 
-  private int FindToolSlotInInventory(EnumTool toolType, IInventory inventory) {
-    for (int i = 0; i < inventory.Count; i++) {
-      if (IsRightTool(inventory[i], toolType)) {
-        return i;
-      }
+        RegisterKey(HOTKEY_SMARTCURSOR, GlKeys.R);
+        RegisterKey(HOTKEY_SMARTCURSOR_TOGGLE, GlKeys.Tilde);
+        _capi.Input.AddHotkeyListener(HotKeyListener);
     }
-    return -1;
-  }
 
-  private bool SwapTool(string inventoryName, EnumTool toolType,
-                        ItemSlot currentSlot) {
-    IInventory inventory =
-        _capi.World.Player.InventoryManager.GetOwnInventory(inventoryName);
+    private void SmartToolStopListListener(float t) {
+        // This should not happen but don't do anything if there's no current smart
+        // tool held
+        if (!_isSmartToolHeld) {
+            return;
+        }
+        int currentActiveSlotIndex = _capi.World.Player.InventoryManager.ActiveHotbarSlotNumber;
 
-    int slotNumber = FindToolSlotInInventory(toolType, inventory);
-
-    if (slotNumber < 0) {
-      return false;
+        // To avoid confusion when active bar change disable the smart tool
+        // or when not in toggle mode and hotkey was released
+        if (currentActiveSlotIndex != _savedActiveSlotIndex ||
+            (!_isToggleMode && !_capi.Input.IsHotKeyPressed(HOTKEY_SMARTCURSOR))) {
+            PopTool();
+        }
     }
-    ItemSlot slot = inventory[slotNumber];
 
-    _savedSlotIndex = slotNumber;
-    _savedSlotInventoryName = inventoryName;
-    _savedActiveSlotIndex =
-        _capi.World.Player.InventoryManager.ActiveHotbarSlotNumber;
+    private EnumTool[] SmartToolSelectorEntity() {
+        EntitySelection es = _capi.World.Player.CurrentEntitySelection;
 
-    bool ret = SwapItemSlot(currentSlot, slot);
-
-    // TODO is this needed?
-    _capi.World.Player.InventoryManager.CloseInventoryAndSync(inventory);
-    return ret;
-  }
-
-  private bool PushTool() {
-    ItemSlot currentSlot = _capi.World.Player.InventoryManager.ActiveHotbarSlot;
-    EnumTool[] tools = SmartToolSelector();
-    if (tools.Length == 0) {
-      return false;
+        if (es != null) {
+            Entity entity = es.Entity;
+            if (!entity.Alive) {
+                return [EnumTool.Knife];
+            }
+        }
+        return [];
     }
-    for (int i = 0; i < tools.Length; i++) {
-      // First Stop if the current tool is the right one
-      if (IsRightTool(currentSlot, tools[i])) {
+
+    // This function return tool based on targeted block
+    private EnumTool[] SmartToolSelector() {
+        // TODO clean this
+        EnumTool[] tools = SmartToolSelectorEntity();
+        if (tools.Length > 0) {
+            return tools;
+        }
+        BlockSelection bs = _capi.World.Player.CurrentBlockSelection;
+
+        if (bs == null) {
+            return [];
+        }
+
+        Block block = _capi.World.BlockAccessor.GetBlock(bs.Position);
+        //_capi.ShowChatMessage($"Material {block.BlockMaterial}");
+        string prefix = block?.Code?.Path is string p ? (p.IndexOf('-') is int i && i >= 0 ? p[..i] : p) : null;
+
+        if (_domainTools.TryGetValue(prefix, out tools)) {
+            return tools;
+        }
+
+        if (_materialTools.TryGetValue(block.BlockMaterial, out tools)) {
+            return tools;
+        }
+
+        return [];
+    }
+
+    private bool SwapItemSlot(ItemSlot a, ItemSlot b) {
+        // Until i know how to do this properly right now i'm using the
+        // mouseItemSlot as temporary item holder during the swap
+        ItemSlot mouseItemSlot = _capi.World.Player.InventoryManager.MouseItemSlot;
+        if (!mouseItemSlot.Empty) {
+            return false;
+        }
+
+        // I really need to find a better way to do this it's seems to work
+        // Now this two move operation are here to account for empty item slot as
+        // well as slot with multiple stack size maybe try with max stack size and
+        // call it a day?
+        ItemStackMoveOperation op_a =
+            new ItemStackMoveOperation(_capi.World, EnumMouseButton.None, 0, EnumMergePriority.ConfirmedMerge, a.StackSize);
+
+        ItemStackMoveOperation op_b =
+            new ItemStackMoveOperation(_capi.World, EnumMouseButton.None, 0, EnumMergePriority.ConfirmedMerge, b.StackSize);
+
+        object obj = _capi.World.Player.InventoryManager.TryTransferTo(a, mouseItemSlot, ref op_a);
+        if (obj != null) {
+            _capi.Network.SendPacketClient(obj);
+        }
+
+        // There's a bug here if item a is a bowl or bucket, and somewhere in your inventory or bar you have
+        // a another bowl or bucket filled with something, the other item will be replaced, it's really hard to explain
+        // but it's clearly looks like a bug in TryTransferTo, i try different operation but it doesn't seems to have any impact
+        if (!a.Empty) {
+            // stop here until i found fix for this bug
+            return false;
+        }
+
+        obj = _capi.World.Player.InventoryManager.TryTransferTo(b, a, ref op_b);
+        if (obj != null) {
+            _capi.Network.SendPacketClient(obj);
+        }
+
+        obj = _capi.World.Player.InventoryManager.TryTransferTo(mouseItemSlot, b, ref op_a);
+        if (obj != null) {
+            _capi.Network.SendPacketClient(obj);
+        }
+
+        return true;
+    }
+
+    private bool IsRightTool(ItemSlot slot, EnumTool toolType) {
+        EnumTool? currentTool = slot?.Itemstack?.Collectible?.Tool;
+        return currentTool == toolType;
+    }
+
+    private int FindToolSlotInInventory(EnumTool toolType, IInventory inventory) {
+        for (int i = 0; i < inventory.Count; i++) {
+            if (IsRightTool(inventory[i], toolType)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private bool SwapTool(string inventoryName, EnumTool toolType, ItemSlot currentSlot) {
+        IInventory inventory = _capi.World.Player.InventoryManager.GetOwnInventory(inventoryName);
+
+        int slotNumber = FindToolSlotInInventory(toolType, inventory);
+
+        if (slotNumber < 0) {
+            return false;
+        }
+        ItemSlot slot = inventory[slotNumber];
+
+        _savedSlotIndex = slotNumber;
+        _savedSlotInventoryName = inventoryName;
+        _savedActiveSlotIndex = _capi.World.Player.InventoryManager.ActiveHotbarSlotNumber;
+
+        bool ret = SwapItemSlot(currentSlot, slot);
+
+        // TODO is this needed?
+        _capi.World.Player.InventoryManager.CloseInventoryAndSync(inventory);
+        return ret;
+    }
+
+    private bool PushTool() {
+        ItemSlot currentSlot = _capi.World.Player.InventoryManager.ActiveHotbarSlot;
+        EnumTool[] tools = SmartToolSelector();
+
+        if (tools.Length == 0) {
+            return false;
+        }
+        for (int i = 0; i < tools.Length; i++) {
+            // First Stop if the current tool is the right one
+            if (IsRightTool(currentSlot, tools[i])) {
+                return false;
+            }
+
+            // Than prioritize hotbar for tool search
+            if (SwapTool(GlobalConstants.backpackInvClassName, tools[i], currentSlot)) {
+                return true;
+            }
+
+            // When not found take the tool from backpack
+            if (SwapTool(GlobalConstants.hotBarInvClassName, tools[i], currentSlot)) {
+                return true;
+            }
+        }
         return false;
-      }
-
-      // Than prioritize hotbar for tool search
-      if (SwapTool(GlobalConstants.backpackInvClassName, tools[i],
-                   currentSlot)) {
-        return true;
-      }
-
-      // When not found take the tool from backpack
-      if (SwapTool(GlobalConstants.hotBarInvClassName, tools[i], currentSlot)) {
-        return true;
-      }
     }
-    return false;
-  }
 
-  private void UnregisterSmartToolStopListListener() {
-    if (_listener >= 0) {
-      _capi.Event.UnregisterGameTickListener(_listener);
-      _listener = -1;
+    private void UnregisterSmartToolStopListListener() {
+        if (_listener >= 0) {
+            _capi.Event.UnregisterGameTickListener(_listener);
+            _listener = -1;
+        }
     }
-  }
-  private void PopTool() {
-    _isSmartToolHeld = false;
-    UnregisterSmartToolStopListListener();
-    IInventory inventory = _capi.World.Player.InventoryManager.GetOwnInventory(
-        _savedSlotInventoryName);
-    IInventory hotbar = _capi.World.Player.InventoryManager.GetOwnInventory(
-        GlobalConstants.hotBarInvClassName);
+    private void PopTool() {
+        _isSmartToolHeld = false;
+        UnregisterSmartToolStopListListener();
+        IInventory inventory = _capi.World.Player.InventoryManager.GetOwnInventory(_savedSlotInventoryName);
+        IInventory hotbar = _capi.World.Player.InventoryManager.GetOwnInventory(GlobalConstants.hotBarInvClassName);
 
-    ItemSlot bar = hotbar[_savedActiveSlotIndex];
-    ItemSlot back = inventory[_savedSlotIndex];
+        ItemSlot bar = hotbar[_savedActiveSlotIndex];
+        ItemSlot back = inventory[_savedSlotIndex];
 
-    SwapItemSlot(bar, back);
-  }
-
-  private void StartSmartCursor(bool mode) {
-    _isToggleMode = mode;
-    if (!_isSmartToolHeld) {
-      UnregisterSmartToolStopListListener();
-      _listener =
-          _capi.Event.RegisterGameTickListener(SmartToolStopListListener, 100);
-      _isSmartToolHeld = PushTool();
-    } else if (_isToggleMode) {
-      PopTool();
+        SwapItemSlot(bar, back);
     }
-  }
+
+    private void StartSmartCursor(bool mode) {
+        _isToggleMode = mode;
+        if (!_isSmartToolHeld) {
+            UnregisterSmartToolStopListListener();
+            _listener = _capi.Event.RegisterGameTickListener(SmartToolStopListListener, 100);
+            _isSmartToolHeld = PushTool();
+        } else if (_isToggleMode) {
+            PopTool();
+        }
+    }
 }
 }
