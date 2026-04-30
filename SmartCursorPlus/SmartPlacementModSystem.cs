1e62765792c873a9af565b26944bf4af8152a473
lguarda <leoman037@live.fr>
2026-04-30 21:48:19 +0200

    code: oups debug message in chat

--- SmartCursorPlus/SmartPlacementModSystem.cs
+++ SmartCursorPlus/SmartPlacementModSystem.cs
@@ -148,7 +148,7 @@ public class SmartPlacement : ModSystem {
         SmartCursorUtils.RayTrace(_capi, new(pos.X, posY, pos.Z), new(dirX, 0, dirZ), 0.3, 5, (p) => {
             BlockPos tmpPos = new BlockPos((int)p.X, (int)p.Y, (int)p.Z);
             Block block = _capi.World.BlockAccessor.GetBlock(tmpPos);
-             _capi.ShowChatMessage($"mat {block.BlockMaterial} {block?.Code?.Path}");
+             // _capi.ShowChatMessage($"mat {block.BlockMaterial} {block?.Code?.Path}");
 
             // see https://apidocs.vintagestory.at/api/Vintagestory.API.Common.Block.html?q=Block#Vintagestory_API_Common_Block_Replaceable
             // 6000 is the limit for tallgrass, then it include water lava and air of course
